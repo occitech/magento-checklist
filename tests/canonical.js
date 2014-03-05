04e9b506d68b18772b8772c0150b47f8b41c0def
Damien Biasotto <damien@occi-tech.com>
2014-03-05 10:57:43 +0100

    Avoid test failing if no category set.

--- tests/canonical.js
+++ tests/canonical.js
@@ -5,13 +5,16 @@ casper.test.begin('Canonical Link is set on categories pages', _app.categories.l
 	var categoriesUrls = _app.categories.map(function(slug) {
 		return category.url(slug);
 	});
-    casper.start().each(categoriesUrls, function(self, link) {
-		self.thenOpen(link, function() {
-			utils.dump(link);
-			test.assertExists('link[rel=canonical]', "Canonical link is set");
+	if (categoriesUrls.length == 0) {
+		test.skip(1, 'No category found.');
+	} else {
+		casper.start().each(categoriesUrls, function(self, link) {
+			self.thenOpen(link, function() {
+				test.assertExists('link[rel=canonical]', "Canonical link is set");
+			})
 		})
-	})
-	.run(function() {
-		test.done();
-    });
+		.run(function() {
+			test.done();
+		});
+	}
 });
